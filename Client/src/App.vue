43cb4f83a1722f8146a212cd5791ef1db92d42fa
duynstee <74192436+duynstee@users.noreply.github.com>
2026-01-23 15:35:47 +0100

    Update App.vue

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -7,7 +7,7 @@
 <script setup>
 import { RouterLink, RouterView, useRouter } from "vue-router";
 import { logout } from "../src/services/authService";
-import AppNavBar from "./components/AppNavbar.vue";
+import AppNavBar from "./components/AppNavBar.vue";
 
 const router = useRouter();
 </script>
